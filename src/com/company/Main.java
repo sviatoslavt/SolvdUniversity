78ab2be5e34b5869a6cbb5073e6392e49d1ba1aa
Sviatoslav <73437401+SviatoslavT@users.noreply.github.com>
2022-03-24 20:43:46 +0200

    just update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,26 @@ public class Main {
         int submitted_works = student1.getSubmitted_works();
 
 
-            if(submitted_works == 0 ){
+        switch (submitted_works){
+            case submitted_works <= 1:
                 System.out.println("The student: " + student1.getRecord_book_number() + " not certified and passed: " + submitted_works + " works.");
-            }
-            else if(submitted_works == 2){
+                break;
+
+            case submitted_works == 2:
                 System.out.println("The student: " + student1.getRecord_book_number() + " rating: 3 and passed: " + submitted_works + " works.");
-            }
-            else if(submitted_works == 3){
+                break;
+
+            case submitted_works == 3:
                 System.out.println("The student: " + student1.getRecord_book_number() + " rating: 4 and passed: " + submitted_works + " works.");
-            }
-            else if(submitted_works >= 4){
+                break;
+
+            case submitted_works >= 4:
                 System.out.println("The student: " + student1.getRecord_book_number() + " rating: 5 and passed: " + submitted_works + " works.");
-            }
+                break;
+
+            default:
+                break;
+        }
+
     }
 }
